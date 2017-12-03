7a324552f6490b18f4a3abdce6d05d3b1435bf28
yokddj <yokddj@gmail.com>
2017-12-03 17:00:34 +0100

    Domingo 3
    Corrección de un error en la pagina principal e insercion de la sección carrito.

---
+++ carrito.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php 
+
+		include 'contenido_head.html';
+ 	?>
+ 	<META NAME="title" CONTENT="Alquila un Sobrino: Carrito"> 
+	<title>CARRITO</title>
+</head>
+<body>
+
+	<!--    AGREGAMOS LA CABECERA CON PHP	-->
+
+	<?php 
+
+		include 'cabecera.html';
+ 	?>
+
+
+	<?php 
+
+		include 'footer.html';
+ 	?>
+</body>
+</html>
+
+
+
+
+
+
+
