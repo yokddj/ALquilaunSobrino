05c9d671027de8c9eaa70a9e2be71c74643877ce
yokddj <yokddj@gmail.com>
2017-12-12 13:46:35 +0100

    Avance sesion Admin
    Ya estan hechas las tablas de edición, pero aun no funcionan.

--- editar_usuarios.php
+++ editar_usuarios.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include 'conexionBD.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -37,7 +38,47 @@ session_start();
 			
 			<section>
 				<center>
-				
+					<h1>MODIFICAR Y/O ELIMINAR</h1>
+					<table >
+						<tr>
+							<td>Login</td>
+							<td>Password</td>
+							<td>Nombre</td>
+							<td>Apellido</td>
+							<td>email</td>
+							<td>Telefono</td>
+							<td>Dirección</td>
+							<td>Ciudad</td>
+							<td>Fecha de nacimiento</td>
+							<td>Privilegio</td>
+							<td>Eliminar</td>
+							<td>Modificar</td>
+						</tr>
+						<?php 
+						
+								$query="select * from usuarios";	
+						$resultado=mysqli_query($bd,$query);
+						while($fila=mysqli_fetch_array($resultado)){
+							echo '
+							<tr>
+							<td><input type="text" class="login" value="'.$fila['login'].'"></td>
+							<td><input type="text" class="password" value="'.$fila['password'].'"></td>
+							<td><input type="text" class="nombre" value="'.$fila['nombre'].'"></td>
+							<td><input type="text" class="apellido" value="'.$fila['apellido'].'"></td>
+							<td><input type="text" class="email" value="'.$fila['email'].'"></td>
+							<td><input type="text" class="telefono" value="'.$fila['telefono'].'"></td>
+							<td><input type="text" class="direccion" value="'.$fila['direccion'].'"></td>
+							<td><input type="text" class="ciudad" value="'.$fila['ciudad'].'"></td>
+							<td><input type="text" class="fecha_nacimiento" value="'.$fila['fecha_nacimiento'].'"></td>
+							<td><input type="text" class="privilegio" value="'.$fila['privilegio'].'"></td>
+							<td><button class="eliminar" data-login="'.$fila['login'].'">Eliminar</button></td>
+							<td><button class="modificar" data-login="'.$fila['login'].'">Modificar</button></td>
+							</tr>
+							';
+						}
+						?>
+					</table>
+				<p>Nota: El privilegio será C para un usuario normal y A para el administrador</p>
 				</center>
 
 			</section>
