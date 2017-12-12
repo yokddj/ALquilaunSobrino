130091f0f733b633920e3e82f307ded1a66da666
mariosavior <34134824+mariosavior@users.noreply.github.com>
2017-12-12 12:47:12 +0100

    Cambio en verificador

--- verificador.php
+++ verificador.php
@@ -16,12 +16,10 @@
 			//Aqui comprobaremos si es un usuario normal o el admin
 				if($array[0]['Privilegio']=="A"){
 					$_SESSION['Usuario']=$array;
-					header("Location: index.php");
 				}else{
 					$_SESSION['Usuario']=$array;
-					header("Location: index.php");
-
 				}
+				header("Location: index.php");
 				//$_SESSION['Usuario']=$array;
 				//echo "Hola ".$array[0]['Nombre']." con apellido ".$array[0]['Apellido']." Te has registrado como: ".$array[0]['Privilegio']."";
 				
